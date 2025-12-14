ebfd7d163fe61206a5ce3c3e0ca6cbb3c395e9eb
kevinjavascript <fauna_wu@qq.com>
2025-12-14 08:31:34 +0000

    Deploying to gh-pages from @ kevinjavascript/kevinjavascript.github.io@502b2a9e0a16fddaaeb177d084de0741d051d665 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -113,7 +113,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%66%61%75%6E%61_%77%75@%71%71.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
